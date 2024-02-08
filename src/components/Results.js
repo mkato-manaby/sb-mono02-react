58e96a6f633a967f811c7f196588de95bb9ffe28
mkato-manaby <mkato.manaby@gmail.com>
2024-02-08 13:01:38 +0900

    p135まで終了

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,14 @@
-const Results = (props) => {
+const Results = ({results}) => {
+    const { cityName, country, temperature, conditionText, icon } = results;
     return (
         <div>
-            {props.results.cityName && <div>{props.results.cityName}</div>}
-            {props.results.country && <div>{props.results.country}</div>}
-            {props.results.temperature && <div>{props.results.temperature} <span>°C</span></div>}
-            {props.results.conditionText &&
-                <div>
-                    <img src={props.results.icon} alt="icon" />
-                    <span>{props.results.conditionText}</span>
+            {cityName && <div className="results-city">{cityName}</div>}
+            {country && <div className="results-country">{country}</div>}
+            {temperature && <div className="results-temp">{temperature} <span>°C</span></div>}
+            {conditionText &&
+                <div className="results-condition">
+                    <img src={icon} alt="icon" />
+                    <span>{conditionText}</span>
                 </div>
             }
         </div>
