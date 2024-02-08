6c25af0c119a740de63d658d8c5bb5e4b75e547d
mkato-manaby <mkato.manaby@gmail.com>
2024-02-08 13:50:24 +0900

    修正

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 const Form = ({ city, setCity, getWeather }) => {
     return (
         <form onSubmit={getWeather}>
-            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} value={city} />
+            <input type="text" name="city" placeholder="英語で都市名を入力" onChange={e => setCity(e.target.value)} value={city} />
             <button type="submit">Get Weahter</button>
         </form>
     );
