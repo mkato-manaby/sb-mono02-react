33bc053c23b372e5e0ab3d0de3e3e5172aeb6f2f
mkato-manaby <mkato.manaby@gmail.com>
2024-02-08 11:40:35 +0900

    P124まで終了

--- src/App.js
+++ src/App.js
@@ -7,17 +7,36 @@ import './App.css';
 
 function App() {
   const [city, setCity] = useState("");
+  const [results, setResults] = useState({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
+
   const getWeather = (e) => {
     e.preventDefault();
-    axios.get("https://api.weatherapi.com/v1/current.json?key=17205c083433440b88624633240702&q=London&aqi=no")
-      .then(res => console.log(res))
+    axios.get(`https://api.weatherapi.com/v1/current.json?key=17205c083433440b88624633240702&q=${city}&aqi=no`)
+      .then(res => {
+        setResults({
+          country: res.data.location.country,
+          cityName: res.data.location.name,
+          temperature: res.data.current.temp_c,
+          conditionText: res.data.current.condition.text,
+          icon: res.data.current.condition.icon,
+        })
+      })
   }
   return (
-    <div className="test">
-      <Title />
-      <Form setCity={setCity} getWeather={getWeather} />
-      <Results />
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
     </div>
+
   );
 };
 
